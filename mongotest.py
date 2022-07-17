9af21ef26ba9813ba30d360a18894abd8b3d3e9a
ks <Luci1108$>
2022-07-17 19:28:29 +0530

    This is my Commit change for the MongoDb

--- mongotest.py
+++ mongotest.py
@@ -11,6 +11,26 @@ d={
     "surname":"kamalakannan"
 }
 
+d1={
+    "name":"prasath",
+    "email":"prasathks20@gmail.com",
+    "surname":"kamalakannan"
+}
+d2={
+    "name":"prasath",
+    "email":"prasathks20@gmail.com",
+    "surname":"kamalakannan"
+}
+d3={
+    "name":"prasath",
+    "email":"prasathks20@gmail.com",
+    "surname":"kamalakannan"
+}
+d4={
+    "name":"prasath",
+    "email":"prasathks20@gmail.com",
+    "surname":"kamalakannan"
+}
 db1=client['mongotest']
 coll=db1['test']
 coll.insert_one(d)
